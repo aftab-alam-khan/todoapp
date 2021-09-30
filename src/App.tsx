9d10bd1f77921bb1a1fad5a13d380aab6d9fb448
Aftab Alam Khan <aftab.khan@pager.com>
2021-09-30 19:31:21 -0400

    delete todo from list

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,37 @@
 
 import { useState } from 'react';
 
+
+
 function App() {
 
-  // let newToDo = "test new to do";
-
-  const [newToDo, setNewToDo] = useState("test new to do")
-
+  const [, setNewToDo] = useState("test new to do")
   const [inputText, setInputText] = useState('')
-
-  let disabledButton = (inputText.length === 0) 
-
-
-
-  console.log('outside return', newToDo);
+  const [toDoList, setToDoList] = useState<string[]>([])
+  let disabledButton = (inputText.length === 0)
   
-
+  
+  
   return (
     
     <div>
       <input type='text' name='todo' placeholder='Enter your todo' onChange={(event) => {
-        // console.log(event.target.value);
         setInputText(event.target.value);
-        
-        
       }} value={inputText}></input>
       
       <button onClick={() => {
-        // const elementTextBox = document.querySelector('[name="todo"]') as any;
-        
-        // setNewToDo(elementTextBox.value);
         setNewToDo(inputText);
-
+        setToDoList([...toDoList, `${inputText}`])
+        
         setInputText('');
-        
-        console.log(newToDo);
-        
-      }} disabled={ disabledButton }>Add ToDo</button>
-      { newToDo }
+      }} disabled={disabledButton}>Add ToDo</button>
+      <ul>
+      {toDoList.map((l, i) => 
+        <li key={i} onClick={() => {
+          setToDoList(toDoList.filter((cre, ind) => i !== ind));
+        }}>{l}</li>)
+      }
+      </ul>
       </div>
   );
 }
