23c6bbb09298f2c1da1ac6b9f6fc4825ca677a50
Aftab Alam Khan <aftab.khan@pager.com>
2021-09-30 09:54:30 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import { useState } from 'react';
 
 function App() {
+
+  // let newToDo = "test new to do";
+
+  const [newToDo, setNewToDo] = useState("test new to do")
+
+  const [inputText, setInputText] = useState('')
+
+  let disabledButton = (inputText.length === 0) 
+
+
+
+  console.log('outside return', newToDo);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div>
+      <input type='text' name='todo' placeholder='Enter your todo' onChange={(event) => {
+        // console.log(event.target.value);
+        setInputText(event.target.value);
+        
+        
+      }} value={inputText}></input>
+      
+      <button onClick={() => {
+        // const elementTextBox = document.querySelector('[name="todo"]') as any;
+        
+        // setNewToDo(elementTextBox.value);
+        setNewToDo(inputText);
+
+        setInputText('');
+        
+        console.log(newToDo);
+        
+      }} disabled={ disabledButton }>Add ToDo</button>
+      { newToDo }
+      </div>
   );
 }
 
